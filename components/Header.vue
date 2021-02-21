0fd996f3bf6cd631bac03d404ec1ed0a97dc0dfd
aimkiray <root@meowwoo.com>
2021-02-21 19:19:05 +0800

    refactor: cat ear calculator

--- components/Header.vue
+++ components/Header.vue
@@ -1,109 +1,99 @@
 <template>
-  <div class="header-wrapper">
-    <div class="title-wrapper">
-      <router-link to="/" class="title">{{ $site.title }}</router-link>
-    </div>
-    <div v-if="$themeConfig.nav" class="nav-wrap">
-      <component
-        v-for="item in $themeConfig.nav"
-        :key="item.text"
-        class="nav-item"
-        v-bind="linkProps(item.link)"
-        >{{ item.text }}
-      </component>
-    </div>
-    <div class="cat-ear">
-      <span class="a-ear">▲</span>
-      <span class="a-ear">▲</span>
-    </div>
-  </div>
+	<div class="header-wrapper">
+		<div class="title-wrapper">
+			<router-link to="/" class="title">{{ $site.title }}</router-link>
+		</div>
+		<div v-if="$themeConfig.nav" class="nav-wrap">
+			<component
+				v-for="item in $themeConfig.nav"
+				:key="item.text"
+				class="nav-item"
+				v-bind="linkProps(item.link)"
+				>{{ item.text }}
+			</component>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  methods: {
-    linkProps(url) {
-      if (url.match(/^(http(s)?|ftp):\/\//)) {
-        return {
-          is: "a",
-          href: url,
-          target: "_blank",
-          rel: "noopener",
-        };
-      }
-      return {
-        is: "router-link",
-        to: url,
-      };
-    },
-  },
+	methods: {
+		linkProps(url) {
+			if (url.match(/^(http(s)?|ftp):\/\//)) {
+				return {
+					is: "a",
+					href: url,
+					target: "_blank",
+					rel: "noopener",
+				};
+			}
+			return {
+				is: "router-link",
+				to: url,
+			};
+		},
+	},
 };
 </script>
 
 <style lang="stylus">
 .header-wrapper {
-  padding: 2rem 0 1rem 0;
-  height: 5rem;
-  // border-bottom: solid 0.5rem #fc8181;
-  background-color: white;
+	padding: 1rem 1rem;
+	height: 5rem;
+	background-color: white;
 
-  .title {
-    font-family: 'Press Start 2P';
-    display: inline-block;
-    padding: 0 0.3rem 0 0.5rem;
-    margin-bottom: 0.5rem;
-    border-right: 0.75rem solid $link-color;
-    animation: blink 1s infinite;
-    font-size: 1.5rem;
-    text-decoration: underline;
+	.title-wrapper {
+		width: 10rem;
+        margin-right: calc(100% - 22rem);
+        display: inline-block;
 
-    @media screen and (max-width: $mobile-width) {
-      font-size: 1rem;
-      border-right: 0.5rem solid $link-color;
-    }
-  }
+		.title {
+			font-family: $font-title;
+			padding: 0 0.3rem 0 0.5rem;
+			border-right: 0.75rem solid $link-color;
+			animation: blink 1s infinite;
+			font-size: 2.5rem;
+			text-decoration: underline;
 
-  @keyframes blink {
-    0% {
-      border-right-color: transparent;
-    }
+			@media screen and (max-width: $mobile-width) {
+				font-size: 2rem;
+				border-right: 0.5rem solid $link-color;
+			}
+		}
+	}
 
-    50% {
-      border-right-color: transparent;
-    }
+	@keyframes blink {
+		0% {
+			border-right-color: transparent;
+		}
 
-    51% {
-      border-right-color: $link-color;
-    }
+		50% {
+			border-right-color: transparent;
+		}
 
-    100% {
-      border-right-color: $link-color;
-    }
-  }
+		51% {
+			border-right-color: $link-color;
+		}
 
-  .nav-item {
-    font-family: $fonts-serif;
-    font-size: 0.875rem;
-    padding-left: 0.5rem;
-    color: $accent-color;
-  }
+		100% {
+			border-right-color: $link-color;
+		}
+	}
 
-  .cat-ear {
-    font-family: arial;
-    text-transform: uppercase;
-    text-align: center;
-    letter-spacing: 3rem;
-  }
+	.nav-wrap {
+		max-width: 20rem;
+        display: inline-block;
 
-  .a-ear {
-    letter-spacing: 0;
-    text-shadow: 0.5rem -0.02rem #fc8181, -0.5rem -0.02rem #fc8181, 0 -1.04rem #fc8181;
-    color: #FAFAFA;
-    font-size: 6rem;
-    line-height: 4rem;
-    z-index: 1;
-    position: relative;
-    bottom: 1.75rem;
-  }
+		.nav-item {
+			font-family: $font-title;
+			font-size: 1rem;
+			padding-left: 0.5rem;
+			color: $accent-color;
+		}
+
+        @media screen and (max-width: $mobile-width) {
+			display: block;
+		}
+	}
 }
 </style>
--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="global-layout">
-    <Header class="header" />
+	<div class="global-layout">
+		<Header class="header" />
 
-    <div class="main-wrapper">
-      <DefaultGlobalLayout />
-    </div>
+		<div class="cat-ear">
+			<span class="a-ear">▲</span>
+			<span class="a-ear">▲</span>
+		</div>
 
-    <Footer class="footer" />
-  </div>
+		<DefaultGlobalLayout class="main-wrapper" :style="borderRadius" />
+
+		<Footer class="footer" />
+	</div>
 </template>
 
 <script>
@@ -16,30 +19,90 @@ import Header from "@theme/components/Header.vue";
 import Footer from "@theme/components/Footer.vue";
 
 export default {
-  components: {
-    DefaultGlobalLayout: GlobalLayout,
-    Header,
-    Footer,
-  },
+	components: {
+		DefaultGlobalLayout: GlobalLayout,
+		Header,
+		Footer,
+	},
+	data() {
+		return {
+            screenWidth: document.body.clientWidth,
+            radiusSize: 30,
+			borderRadius: {
+                borderTopLeftRadius: "49% 32px",
+				borderTopRightRadius: "49% 32px",
+            },
+		};
+	},
+	mounted() {
+        const radiusInfo = "49% " + parseInt(((document.body.clientWidth - 485) * 215 + 15520) / document.body.clientWidth) + "px"
+        this.borderRadius.borderTopLeftRadius = radiusInfo
+        this.borderRadius.borderTopRightRadius = radiusInfo
+		const that = this;
+		window.onresize = () => {
+			return (() => {
+				window.screenWidth = document.body.clientWidth;
+                that.screenWidth = window.screenWidth;
+			})();
+		};
+	},
+	watch: {
+		screenWidth(val) {
+			if (!this.timer) {
+				this.screenWidth = val;
+                this.timer = true;
+				let that = this;
+				setTimeout(function () {
+                    const radiusInfo = "49% " + parseInt(((document.body.clientWidth - 485) * 215 + 15520) / document.body.clientWidth) + "px"
+                    that.borderRadius.borderTopLeftRadius = radiusInfo
+                    that.borderRadius.borderTopRightRadius = radiusInfo
+					that.timer = false;
+				}, 500);
+			}
+		},
+	},
 };
 </script>
 
 <style lang="stylus">
 .global-layout {
-    display: flex;
-    height: 100vh;
-    flex-direction: column;
+	display: flex;
+	height: 100vh;
+	flex-direction: column;
 }
 
-.header,
-.footer {
-  flex: none;
+.header, .footer {
+	flex: none;
 }
 
 .main-wrapper {
-    flex: 1 0 auto;
-    padding-top 4rem
-    padding-bottom 8rem
-    background-color #FAFAFA
+	flex: 1 0 auto;
+	padding-top: 8rem;
+	background: no-repeat linear-gradient($bg-color, #fff);
+	background-size: 100% 20rem;
+    box-shadow: rgba(0, 0, 0, 0.01) 0 -0.1rem 0.1rem,
+                rgba(0, 0, 0, 0.04) 0 -0.25rem 0.25rem,
+                rgba(0, 0, 0, 0.04) 0 -1rem 1rem,
+                rgba(0, 0, 0, 0.01) 0 -1.5rem 1.5rem;
+}
+
+.cat-ear {
+	font-family: arial;
+	text-transform: uppercase;
+	text-align: center;
+	letter-spacing: 3rem;
+	position: relative;
+	bottom: 2.5rem;
+	height: 0;
+    text-shadow: rgba(0, 0, 0, 0.01) 0 -0.1rem 0.1rem,
+                rgba(0, 0, 0, 0.04) 0 -0.5rem 0.5rem;
+}
+
+.a-ear {
+	letter-spacing: 0;
+	color: $bg-color;
+	font-size: 10rem;
+	line-height: 0;
+	z-index: 1;
 }
 </style>
