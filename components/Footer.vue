919cf495df45df0ffde40ad2195b7adaeede1cb4
aimkiray <root@meowwoo.com>
2021-02-21 19:15:25 +0800

    fix: copyright info

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,67 +1,52 @@
 <template>
-    <div>
-        <!-- <div class="cat-ear">
+	<div>
+		<!-- <div class="cat-ear">
             <span class="a-ear">▲</span>
             <span class="a-ear">▲</span>
         </div> -->
-        <div class="copyright">
+		<div class="copyright">
+			<p>
+				© 2014 - 2200
+				<a href="https://meowwoo.com">aimkiray</a>.
+			</p>
             <p>
-                © 2014 - 2200
-                <a href="https://meowwoo.com">Aimkiray</a>.
-            </p>
-            <p>
-                Theme by
-                <a
-                    href="https://github.com/aimkiray/vuepress-theme-palette"
-                    target="_blank"
-                >Palette</a>.
-                <span></span>
-            </p>
-        </div>
-    </div>
+				Built with ♥ and VuePress. 
+				Designed by
+				<a
+					href="https://github.com/aimkiray/vuepress-theme-palette"
+					target="_blank"
+					>aimkiray</a
+				>.
+				<span></span>
+			</p>
+		</div>
+	</div>
 </template>
 
 <style lang="stylus" scoped>
 .copyright {
-    font-size: 0.875rem;
-    // border-top: solid 0.5rem #fc8181;
-    background-color: #FFEFEF;
-    color: #fc8181;
-    height: 5rem;
-    padding: 0.5rem 0 0 0.5rem;
+	font-size: 1rem;
+	// background: linear-gradient(to top, $bg-color, #fff);
+	color: #fc8181;
+	text-align: center;
+	height: 5rem;
+	padding: 5rem 0 0 0.5rem;
+    display block;
 
-    p {
-        margin: 0;
-        font-family: $fonts-serif;
-        font-weight: 600;
-    }
+	p {
+		margin: 0;
+		font-family: $fonts-sans-serif;
+		font-weight: 400;
+	}
 
-    a {
-        font-family: $fonts-serif;
-        font-weight: 800;
-    }
+	a {
+		font-family: $fonts-sans-serif;
+		font-weight: 600;
+	}
 
-    span {
-        font-family: $fonts-sans-serif;
-        font-weight: 400;
-    }
+	span {
+		font-family: $fonts-sans-serif;
+		font-weight: 400;
+	}
 }
-
-// .cat-ear {
-//     font-family: arial;
-//     text-transform: uppercase;
-//     text-align: center;
-//     letter-spacing: 3rem;
-// }
-
-// .a-ear {
-//     letter-spacing: 0;
-//     text-shadow: 0.5rem 0 #fc8181, -0.5rem 0 #fc8181, 0 -1rem #fc8181;
-//     color: #FFEFEF;
-//     font-size: 6rem;
-//     line-height: 4rem;
-//     z-index: 1;
-//     position: relative;
-//     bottom: -1.25rem;
-// }
 </style>
