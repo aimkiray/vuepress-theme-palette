14f0182d8ee42efc3aa5616a9be9f116abec3ebf
aimkiray <root@meowwoo.com>
2021-02-23 23:06:24 +0800

    refactor: tag layout

--- layouts/FrontmatterKey.vue
+++ layouts/FrontmatterKey.vue
@@ -1,38 +1,123 @@
 <template>
-    <div class="post-tags">
-        <router-link
-            class="post-tag"
-            v-for="tag in $frontmatterKey.list"
-            :key="tag.name"
-            :to="tag.path"
-        >#{{ tag.name }}</router-link>
-    </div>
+	<div>
+		<div class="tag-wrapper">
+			<div class="tags">
+				<router-link
+					class="tag"
+					v-for="tag in $frontmatterKey.list"
+					:key="tag.name"
+					:to="tag.path"
+					:tag-weight="tag.pages.length"
+					tag="div"
+					>{{ tag.name }}
+				</router-link>
+			</div>
+		</div>
+
+		<div class="nyaa-wrapper">
+			<span class="nyaa-left"></span><span class="nyaa-right"></span>
+		</div>
+	</div>
 </template>
 
+<script>
+export default {
+	mounted() {
+		// config
+		const fontMin = 15; //px
+		const fontMax = 35; //px
+		const valueAttributeName = "tag-weight"; // tag in which we puted value
+		const tagsSelector = "[tag-weight]"; // tags elemnts selector
+		const computeColor = true; // flag, if true color will be computed with size
+
+		// mechanism
+		const values = [];
+		document.querySelectorAll(tagsSelector).forEach((tag) => {
+			const tagValue = tag.getAttribute(valueAttributeName);
+
+			values.push({
+				el: tag,
+				val: Number(tagValue),
+			});
+		});
+
+		const valuesSorted = values.sort((a, b) => a.val - b.val);
+		const valueMax = valuesSorted[valuesSorted.length - 1].val;
+
+		valuesSorted.forEach((x) => {
+			const { val, el } = x;
+
+			const fontSize = Math.floor(
+				(val / valueMax) * (fontMax - fontMin + 1) + fontMin
+			);
+
+			if (computeColor) {
+				const color = Math.abs(Math.floor((val / valueMax) * 200 - 200))
+					.toString(16)
+					.repeat(3);
+
+				el.style.color = `#${color}`;
+			}
+
+			el.style.fontSize = `${fontSize}px`;
+		});
+	},
+};
+</script>
+
 <style lang="stylus" scoped>
-.post-tags {
-    text-align: center;
-    padding: 2rem 1rem;
+.tag-wrapper {
+	display: flex;
+	justify-content: center;
 
-    .post-tag {
-        display: inline-block;
-        position: relative;
-        padding: 4px 10px 4px 10px;
-        margin-right: 0.5rem;
-        margin-bottom: 0.5rem;
-        font-family $fonts-serif
-        font-size: 14px;
-        border: 2px solid $link-color;
-        border-radius: 2px;
-        color: $gray-text-color;
+	.tags {
+		width: 900px;
+		text-align: center;
 
-        &:hover {
-            color: $link-color;
-        }
+		div {
+			display: inline-block;
+			line-height: 1.2em;
+			vertical-align: middle;
+			margin: 0 0.2em;
+			transition: color 0.12s ease;
+			cursor: pointer;
+			user-select: none;
 
-        &:last-child {
-            margin-right: 0;
-        }
-    }
+			&:hover {
+				color: $border-color !important;
+			}
+
+			&:active {
+				transform: translateY(4px);
+			}
+		}
+	}
+}
+
+.nyaa-wrapper {
+	text-align: center;
+    padding-top: 2rem;
+
+	.nyaa-left {
+		position: relative;
+		left: 0.2rem;
+		display: inline-block;
+		width: 3rem;
+		height: 1.5rem;
+		border: 0.4rem solid $border-color;
+		border-radius: 0 0 100% 100% / 0 0 200% 160%;
+		border-top: none;
+	}
+
+	.nyaa-right {
+		position: relative;
+		right: 0.2rem;
+		display: inline-block;
+		width: 3rem;
+		height: 1.5rem;
+		border: 0.4rem solid $border-color;
+		border-radius: 0 0 100% 100% / 0 0 160% 200%;
+		border-top: none;
+	}
 }
 </style>
